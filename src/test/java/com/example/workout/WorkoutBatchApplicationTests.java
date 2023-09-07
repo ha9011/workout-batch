1d020db7695d47dd719abb676e6eb5226301433f
ha9011 <gkehddnjs90@gmail.com>
2023-09-07 16:25:43 +0900

    init

---
+++ src/test/java/com/example/workout/WorkoutBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
